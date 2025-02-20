52dd507dd1ca333ae5876bfd9c76debbbe5e2d8c
Ndranto Mael <Ndrantomael39@gmail.com>
2025-02-20 07:12:21 +0300

    Modifications des composantes de la responsivité de la composante profile v2

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-center">
-    <div class="w-80 h-80 rounded-full overflow-hidden border-4 border-gray-600 mt-32 sm:mt-36 md:mt-0">
+    <div class="w-80 h-80 rounded-full overflow-hidden border-4 border-gray-600 mt-44 sm:mt-48 md:mt-0">
       <img src="/src/images/profile.png" alt="Photo de Ndranto Maël" class="w-full h-full object-cover" />
     </div>
   </div>
